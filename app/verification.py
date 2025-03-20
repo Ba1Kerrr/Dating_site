ccfcb028579ed0bd148dd375e08354b6bb661246
C++ | PY  ? <140811719+Ba1Kerrr@users.noreply.github.com>
2025-03-20 13:23:45 +0300

    Update verification.py

--- app/verification.py
+++ app/verification.py
@@ -3,32 +3,26 @@ import requests
 import json
 import random
 import logging
+from dotenv import load_dotenv
+load_dotenv()
 
 # Настройки логирования
 logging.basicConfig(level=logging.INFO)
 
 # Константы
-BREVO_API_KEY = "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"
-BREVO_SERVER = 'https://api.brevo.com/v3/smtp/email'
-SENDER_EMAIL = 'ssfs9943@gmail.com'
-SENDER_NAME = 'SoulMates'
-SUBJECT = 'Verification'
+brevo_api_key = os.environ['TG_key']
+brevo_server = 'https://api.brevo.com/v3/smtp/email'
+sender_email = os.environ['email']
+sender_name = 'SoulMates'
+subject = 'Verification'
 
 def send_email(email):
     try:
         # Код для отправки письма
         text_content = key = str(random.randint(100000, 999999))
-
-        # Настройки Brevo
-        brevo_api_key = "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"
-        brevo_server = "https://api.brevo.com/v3/smtp/email"
-
-        # Данные для отправки письма
-        subject = "Verification"
-        
         # Создание запроса
         payload = json.dumps({
-            "sender": {"name": "SoulMates", "email": "ssfs9943@gmail.com"},
+            "sender": {"name": sender_name , "email": sender_email},
             "to": [{"email": email}],
             "subject": subject,
             "textContent": text_content,
